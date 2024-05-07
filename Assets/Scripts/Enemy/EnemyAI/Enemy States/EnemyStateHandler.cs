8732c5012ec499d31cb99949178b0bb4b7b789e6
Raaee <varangurenv@gmail.com>
2024-05-07 11:57:11 -0400

    state scripts init

--- Assets/Scripts/Enemy/EnemyAI/Enemy States/EnemyStateHandler.cs
+++ Assets/Scripts/Enemy/EnemyAI/Enemy States/EnemyStateHandler.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class EnemyStateHandler : MonoBehaviour
 {
-    
+    private EnemyAIState currentState;
+
+    void Start() {
+        EnterState();
+
+    }
+    public void EnterState() {
+        currentState.OnStateEnter();
+    }
+    void Update() {
+        currentState.OnStateUpdate();
+    }
+    public void ChangeState(EnemyAIState newState) {
+        currentState.OnStateExit();
+        currentState = newState;
+        EnterState();
+    } 
 }
---
+++ Assets/Scripts/Enemy/EnemyAI/Enemy States/MoveTowardsCrystalES.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsCrystalES : EnemyAIState 
+{
+    public override void OnStateEnter() {
+
+    }
+    public override void OnStateUpdate() {
+
+    }
+    public override void OnStateExit() {
+
+    }
+}
